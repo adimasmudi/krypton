f7278e52d4fb5d0ed7266dfe9ebc4bb34a4eb118
adimasmudi <adimasmudi@gmail.com>
2021-10-04 22:42:49 +0700

    create counter member

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,35 @@ $(document).ready(function () {
         
     });
 
+        
+    var a = 0;
+    $(window).scroll(function() {
+    var oTop = $('.member').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter-value').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+        $({
+            countNum: $this.text()
+        }).animate({
+            countNum: countTo
+            },
+            {
+            duration: 4000,
+            easing: 'swing',
+            step: function() {
+                $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+                $this.text(this.countNum);
+                //alert('finished');
+            }
+            });
+        });
+        a = 1;
+    }
+    });
+
     //  TESTIMONIALS CAROUSEL
     $("#testimonial-slider").owlCarousel({
         items:1,
@@ -63,6 +92,7 @@ if(preloading){
         preloading.classList.add('hidden');
     },3000);
 }
+
 
 
 
